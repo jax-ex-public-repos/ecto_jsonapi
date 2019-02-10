cf946f0ece24bd3c237a8c449eaec4b74d79c406
Micah Cooper <micah+github@mrmicahcooper.com>
2019-02-10 15:21:58 -0600

    Setup mix files for packaging

--- mix.exs
+++ mix.exs
@@ -4,11 +4,13 @@ defmodule EctoJsonapi.MixProject do
   def project do
     [
       app: :ecto_jsonapi,
+      description: "Use Ecto for JsonApi documents. Simply",
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -27,6 +29,15 @@ defmodule EctoJsonapi.MixProject do
     [
       {:ecto, "~> 3.0"},
       {:jason, "~> 1.1", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      name: "ecto_jsonapi",
+      maintainers: ["Micah Cooper"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mrmicahcooper/ecto_jsonapi"}
     ]
   end
 end
