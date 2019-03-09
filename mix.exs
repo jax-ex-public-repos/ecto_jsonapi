df298f6a72fb76a680fa19d318fd88272c1dbe9a
Micah Cooper <micah+github@mrmicahcooper.com>
2019-03-09 16:42:51 -0600

    wip

--- mix.exs
+++ mix.exs
@@ -4,13 +4,14 @@ defmodule EctoJsonapi.MixProject do
   def project do
     [
       app: :ecto_jsonapi,
-      description: "Use Ecto for JsonApi documents. Simply",
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
-      package: package()
+      source_url: "http://github.com/mrmicahcooper/ecto_jsonapi",
+      package: package(),
+      ocs: docs()
     ]
   end
 
@@ -28,16 +29,24 @@ defmodule EctoJsonapi.MixProject do
   defp deps do
     [
       {:ecto, "~> 3.0"},
-      {:jason, "~> 1.1", only: :test}
+      {:jason, "~> 1.1", only: :test},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 
   defp package do
     [
-      name: "ecto_jsonapi",
       maintainers: ["Micah Cooper"],
-      licenses: ["MIT"],
+      licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/mrmicahcooper/ecto_jsonapi"}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
